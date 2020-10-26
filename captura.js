b0c1ca3eef6a33f3950a642f7fbea9a8958ea837
Geovani Brito <geovani.br@gmail.com>
2020-10-26 00:50:19 -0300

    Validação da URL

--- captura.js
+++ captura.js
@@ -1,21 +1,38 @@
 chrome.contextMenus.create({
-    title: "Capturar thumbnail", 
-    contexts:["image"], 
-    onclick: getVideoCod
-  });
+  title: "Capturar thumbnail",
+  contexts: ["all"],
+  onclick: getVideoCod
+});
 
 function getVideoCod(info) {
-    console.log("Link: " + info.linkUrl);
-    const cod_video = info.linkUrl.split("=");
-    console.log("Código: " + cod_video);
-    if (cod_video[0] == "https://www.youtube.com/watch?v"){
-      getThumbnail(cod_video[1])
-    } else {
-      alert("Vídeo inválido") 
-    }
+  console.log("Link: " + info.linkUrl);
+  var code
+  const code_is_valid = validate_address(info.linkUrl)
+  if (code_is_valid) {
+    code = info.linkUrl.split("v=")[1].split("&")
+    console.log("Código: " + code)
+    getThumbnail(code[0])
+  } else {
+    alert("Não é um vídeo válido")
+  }
 }
 
-function getThumbnail(cod_video){
+function validate_address(url) {
+  var validity
+  if (url == undefined) {
+    validity = false
+  } else {
+    const lista = url.split("/")
+    if (lista[2] == "www.youtube.com") {
+      validity = true
+    } else {
+      validity = false
+    }
+  }
+  return validity
+}
+
+function getThumbnail(cod_video) {
   const url_thumb = "https://img.youtube.com/vi/" + cod_video + "/maxresdefault.jpg"
-  window.open(url_thumb)       
+  window.open(url_thumb)
 }
